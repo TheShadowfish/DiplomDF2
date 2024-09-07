13dfff514eaf02c473ad4806799747a521e31d87
TheShadowfish <shadowfish@yandex.ru>
2024-09-07 12:12:24 +0300

    regular SHIT with path. Hello, Django!

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,45 @@
-from django.db import models
+from django.urls import path
+from django.views.decorators.cache import cache_page
 
-# Create your models here.
+from .views import ClientListView, \
+    MailingListView, MailingCreateView, MailingUpdateView, MailingListViewSend, MailingDeleteView, ClientCreateView, \
+    ClientUpdateView, ClientDeleteView, MailingDetailView, mailing_send, MessageListView, MessageUpdateView, \
+    MessageSettingsUpdateView, MailingSettingsListView, MailingSettingsUpdateView, \
+    toggle_activity_mailing, ClientDetailView, HomePageView
+
+# MessageCreateView,
+
+from restaurant.apps import RestaurantConfig
+
+app_name = RestaurantConfig.name
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='main'),
+
+    # path('mailing_list/', MailingListView.as_view(), name='mailing_list'),
+    # path('mailing_list_send/', MailingListViewSend.as_view(), name='mailing_list_send'),
+    # path('mailing_create/', MailingCreateView.as_view(), name='mailing_create'),
+    # path('mailing_update/<int:pk>/', MailingUpdateView.as_view(), name='mailing_update'),
+    # path('mailing_delete/<int:pk>/', MailingDeleteView.as_view(), name='mailing_delete'),
+    # path('mailing_detail/<int:pk>/', MailingDetailView.as_view(), name='mailing_detail'),
+    #
+    # path('mail_list/', ClientListView.as_view(), name='mail_list'),
+    # path('mail_create/', ClientCreateView.as_view(), name='mail_create'),
+    # path('mail_update/<int:pk>/', ClientUpdateView.as_view(), name='mail_update'),
+    # path('mail_delete/<int:pk>/', ClientDeleteView.as_view(), name='mail_delete'),
+    # path('mail_detail/<int:pk>/', ClientDetailView.as_view(), name='mail_detail'),
+    #
+    # path('mailing_send/<int:pk>/', mailing_send, name='mailing_send'),
+    #
+    # path('message_list/', MessageListView.as_view(), name='message_list'),
+    #
+    # path('message_settings_update/<int:pk>/', MessageSettingsUpdateView.as_view(), name='message_settings_update'),
+    # path('message_update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
+    #
+    # path('settings_list/', MailingSettingsListView.as_view(), name='settings_list'),
+    # path('settings_update/<int:pk>/', MailingSettingsUpdateView.as_view(), name='settings_update'),
+    #
+    # path('mailing_activity/<int:pk>/', toggle_activity_mailing, name='mailing_activity'),
+
+]
+# path('message_create/', MessageCreateView.as_view(), name='message_create'),
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "restaurant/home.html"
+
+    # def get_context_data(self, **kwargs):
+    #     """
+    #     - количество рассылок всего,
+    #     - количество активных рассылок,
+    #     - количество уникальных клиентов для рассылок,
+    #     - три случайные статьи из блога.
+    #
+    #     :param kwargs:
+    #     :return:
+    #     """
+    #     context = super().get_context_data(**kwargs)
+    #     # количество рассылок всего
+    #     context["mailings_count"] = len(Mailing.objects.all())
+    #     # количество активных рассылок
+    #     context["mailings_count_active"] = len(Mailing.objects.filter(settings__status=True))
+    #
+    #     # количество уникальных клиентов для рассылок
+    #     emails_unique = Client.objects.values('email').annotate(total=Count('id'))
+    #     context["emails_unique"] = emails_unique
+    #     context["emails_unique_count"] = len(emails_unique)
+    #     # три случайные статьи из блога
+    #
+    #     # наверняка это можно более оптимально получить, надо спросить как
+    #     # чем весь список статей из базы тянуть
+    #     article_list_len = len(Article.objects.all())
+    #
+    #     # article_list_len_2 = Article.objects.Count()
+    #
+    #     context["article_list_len"] = article_list_len
+    #
+    #     valid_profiles_id_list = Article.objects.values_list('id', flat=True)
+    #     random_profiles_id_list = random.sample(list(valid_profiles_id_list), min(len(valid_profiles_id_list), 3))
+    #     # context["random_articles"] = Article.objects.filter(id__in=random_profiles_id_list)
+    #     # = get_cached_article_list()
+    #     context["random_articles"] = get_cached_article_list().filter(id__in=random_profiles_id_list)
+    #
+    #     # def sample(self, population, k, *, counts=None):
+    #
+    #     return context
